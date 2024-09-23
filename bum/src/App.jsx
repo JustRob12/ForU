ecec6997a7a0bd6085cccd785be559a0b3bd3602
just_rob <roberto.prisoris12@gmail.com>
2024-09-23 19:30:48 +0800

    us

---
+++ bum/src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import React from 'react';
+import './index.css';
+import { ThreeDCardDemo } from './test';
+
+function App() {
+  return (
+    <div>
+      <ThreeDCardDemo />
+    </div>
+  );
+}
+
+export default App;
